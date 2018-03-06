4182b1318f82304dd07cf602d53f2e91fdfddfff
Dan Tilden <historic@outlook.com>
2018-03-06 11:32:36 -0800

    Fixed various numeric display issues

--- app/rpncalculator.js
+++ app/rpncalculator.js
@@ -2,7 +2,7 @@ export default class RPNCalculatorModel {
   constructor() {    
     // the top of the stack (the x register), is stored as a string so we can 
     // easily do things like backspace and "6.00"
-    this._stackTop = '';
+    this._stackTop = '0';
    
     // the rest of the stack (y, z, and t registers)
     this._stack = [0, 0, 0];
@@ -48,18 +48,19 @@ export default class RPNCalculatorModel {
   numberPress(numberStr) {   
     this._clearIfNeeded();
     
-    this._stackTop += numberStr; // append the string onto the buffer
-    
+    if(this._stackTop == '0') {
+      this._stackTop = numberStr;
+    } else {
+      this._stackTop += numberStr;
+    }
+        
     this._lastAction = 'INPUT';
   }
   
   pointPress() {
     this._clearIfNeeded();
     
-    if(this._stackTop == '') {
-      this._stackTop = '0.';
-      this._lastAction = 'INPUT';
-    } else if(this._stackTop.indexOf('.') == -1) {
+    if(this._stackTop.indexOf('.') == -1) {
       this._stackTop += '.';
       this._lastAction = 'INPUT';
     }
@@ -77,7 +78,7 @@ export default class RPNCalculatorModel {
     // if the last thing the user did was press ENTER or use an OPERATOR,
     // then the backspace key should just clear the input
     if(this._lastAction == 'ENTER' || this._lastAction == 'OPERATOR' ) {
-      this._stackTop = '';
+      this._stackTop = '0';
     } else {
       if(this._stackTop.length > 1) {
         this._stackTop = this._stackTop.substring(0, this._stackTop.length - 1);
@@ -133,22 +134,25 @@ export default class RPNCalculatorModel {
   }
   
   getDisplay() {
+    let stackTopFloat = parseFloat(this._stackTop);
+    console.log(this._stackTop);
+    
     if(this._stackTop.length > this._maxDigits) {
       // if the display is too long we must take action
-      
-      let stackTopFloat = parseFloat(this._stackTop);
-      
+      //let displayStr = parseFloat(stackTopFloat.toPrecision(6)).toString();
+            
       //if the number doesn't fit because it's tiny or huge, use scientific notation
       // TODO: make this range dynamically calculated based on maxDigits
-      if(this._stack[0] > 999999 || this._stack[0] < 0.00001) {
+      if(Math.abs(parseFloat(this._stackTop)) > 999999 || Math.abs(parseFloat(this._stackTop)) < 0.00001) {
         let expNotation = stackTopFloat.toExponential(1)
         return (expNotation.length < 8) ? expNotation : 'Error';
       } else {
         // otherwise, we assume that we just need to round it
-        return stackTopFloat.toPrecision(6);
+        console.log(`here! ${parseFloat(stackTopFloat.toPrecision(5)).toString()}`);
+        return parseFloat(stackTopFloat.toPrecision(5)).toString();
       }
     } else {
-      return (this._stackTop == '') ? '0' : this._stackTop;
+      return this._stackTop;
     }
   }
   
@@ -156,11 +160,11 @@ export default class RPNCalculatorModel {
     if(this._lastAction == 'OPERATOR') {
       // if an operator was pressed last, entering a number should shift the registers upward
       this._stack.unshift(parseFloat(this._stackTop));
-      this._stackTop = '';
+      this._stackTop = '0';
       this._stack.pop();
     } else if(this._lastAction == 'ENTER') {
       // if ENTER was just pressed, entering a number should zero the x register
-      this._stackTop = '';
+      this._stackTop = '0';
     }
   }
 }
